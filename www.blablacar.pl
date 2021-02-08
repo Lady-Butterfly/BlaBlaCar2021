acac90ec39fa7b019a359562255dafe9ecdbfb9e
Monika <monika.beata84@gmail.com>
2021-02-08 20:11:36 +0000

    moja strona wersja 1

---
+++ www.blablacar.pl
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+ <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" lang="pl" charset="UTF-8">
+    <title>
+      <img src="C:/Users/Monisia/Desktop/MONIKA/HTML cwiczenia/BlaBlaCar/znaczki-blablacar.png" alt="znaczki blablacar" />Wspolne przejazdy dla kierowcow i pasazerow podrozujacych w te sama strone / BlaBlaCar</title>
+    <style type="text/css">
+
+
+    </style>
+ </head>
+ <body>
+   
+ </body>
+</html>
